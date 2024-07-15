033d94793a641d9666a9319879a6d8b6a0492b62
Chandan Kumar <ck.zero.dev@gamil.com>
2024-07-15 23:10:15 +0530

    first commit

---
+++ app/app.ts
@@ -0,0 +1,16 @@
+import { m } from "@ckzero/maya/web";
+
+const HomePage = () => {
+  return m.Div({
+    children: [
+      m.H2({
+        innerText: "cyfers",
+      }),
+      m.H2({
+        innerText: "cyfers",
+      }),
+    ],
+  });
+};
+
+export const app = HomePage;
